adace2b7d14c0cec6a21174d3d12ec7a2ca171a6
Soomin Shin <ssoomin1@naver.com>
2021-04-20 12:18:45 +0900

    #3 기본 자료형(Data Type) 실습

--- CSBasic/Program.cs
+++ CSBasic/Program.cs
@@ -36,6 +36,41 @@ namespace CSBasic
             Console.Write(" 입니당 \n");
             Console.WriteLine("");
             Console.WriteLine("이건 개행입니다.");
+
+            //자료형(Data Type)
+            //Integer ''이 없으면 기본적으로 int취급
+            Console.WriteLine(53 + 227 - 123);
+            Console.WriteLine(53 * 3 - 3);
+            Console.WriteLine(14 / 2);  //7
+            Console.WriteLine(14 / 3);  //4
+            Console.WriteLine(14 % 3); //2
+
+            //Real Number
+            Console.WriteLine(0);
+            Console.WriteLine(0.0); //0과 0.0은 다르다. 
+            Console.WriteLine(13.123); //double
+            Console.WriteLine(14.0 / 2.0); //7.0
+            Console.WriteLine(14.0 / 3.0); //4.66667
+            Console.WriteLine(14.0 % 3.0); //실수에서는 이러면 안된다.
+
+            Console.WriteLine(14 / 3);
+            Console.WriteLine(14.0 / 3);
+            Console.WriteLine(14 / 3.0);
+            Console.WriteLine(14.0 / 3.0);
+
+            //Character
+            Console.WriteLine('A'); //문자
+            //String
+            Console.WriteLine("B"); //문자열
+            //Escape Character
+            Console.WriteLine("\t 탭 \\ 역슬래시 \n 개행 \" ");
+            Console.WriteLine("미\t림여자정보과학고"); //탭은 한글기준 4글자, 영문 기준 8글자 띄워준다 
+            Console.WriteLine("미림\t여자정보과학고");
+            Console.WriteLine("미림여\t자정보과학고");
+            Console.WriteLine("미림여자정\t보과학고");
+            Console.WriteLine("미림여자정보과\t학고");
+            Console.WriteLine("미림여자정보과학고\t");
+
         }
     }
 }
