10473303ed54793abb9baaf757cc507239343079
Soomin Shin <ssoomin1@naver.com>
2021-04-23 15:52:17 +0900

    #7 자료형 검사 예제

--- CSBasic/Program.cs
+++ CSBasic/Program.cs
@@ -170,6 +170,20 @@ namespace CSBasic
             output3 += " World";
             output3 = output3 + "?!";
             Console.WriteLine(output3);
+
+            int _int = 273;
+            long _long = 522731033265;
+            float _float = 52.273F;
+            double _double = 52.373;
+            char _char = '귤';
+            string _string = " 문자열";
+
+            Console.WriteLine(_int.GetType());
+            Console.WriteLine(_long.GetType());
+            Console.WriteLine(_float.GetType());
+            Console.WriteLine(_double.GetType()) ;
+            Console.WriteLine(_char.GetType());
+            Console.WriteLine(_string.GetType());
         }
     }
 }
