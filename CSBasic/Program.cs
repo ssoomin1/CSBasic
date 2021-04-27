f930d0107abbb1fe16fc4a1fbf2e9a0a9b398528
Soomin Shin <ssoomin1@naver.com>
2021-04-27 12:14:09 +0900

    #10 자료형 변환 예시 - 주의할 점 많음!

--- CSBasic/Program.cs
+++ CSBasic/Program.cs
@@ -206,6 +206,88 @@ namespace CSBasic
                 else
                     Console.WriteLine(input);
             }
+            var a1 = (int)10.0;
+            var b1 = (float)10;
+            var c1 = (double)10;
+
+
+            long longNumber = 2147483637L + 2147483637L;
+            int intNumber = (int)longNumber;
+            Console.WriteLine(intNumber);
+
+            long longNumber2 = 52773;
+            int intNumber2 = (int)longNumber2;
+            Console.WriteLine(intNumber2);
+
+            Console.WriteLine("int,long,float,double 맥스벨류");
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(long.MaxValue); 
+            Console.WriteLine(float.MaxValue); //3.4E+38 = 3.4 * 10의 38승
+            Console.WriteLine(double.MaxValue);
+
+            Console.WriteLine((52).ToString());
+            Console.WriteLine((52.273).ToString());
+            Console.WriteLine(('a').ToString());
+            Console.WriteLine((true).ToString());
+            Console.WriteLine((false).ToString());
+
+            Console.WriteLine((52).ToString().GetType());
+            Console.WriteLine((52.273).ToString().GetType());
+            Console.WriteLine(('a').ToString().GetType());
+            Console.WriteLine((true).ToString().GetType());
+            Console.WriteLine((false).ToString().GetType());
+
+            //잘못된 형변환
+            try
+            {
+                Console.WriteLine(int.Parse("52.273"));
+                Console.WriteLine(int.Parse("abc"));
+                Console.WriteLine(int.Parse("육십삼"));
+                Console.WriteLine(int.Parse("Seven"));
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine("숫자를 입력하세요: " + e.Message);
+            }catch(Exception e)
+            {
+                Console.WriteLine("알 수 없는 에러: " + e.Message);
+            }
+
+            (10).ToString();
+            (52.383).ToString();
+
+            double numberToString = 52.273103;
+            Console.WriteLine(numberToString.ToString("0.0")); //시험 문제 내기 딱 좋다. => 52.3으로 나온다. 반올림 가능
+            Console.WriteLine(numberToString.ToString("0.00"));
+            Console.WriteLine(numberToString.ToString("0.000"));
+            Console.WriteLine(numberToString.ToString("0.0000"));
+
+            //숫자와 문자열 덧셈
+            Console.WriteLine(52 + 273);
+            Console.WriteLine("52" + 273);
+            Console.WriteLine(52 + "273");
+            Console.WriteLine(52+2+273);
+            Console.WriteLine(52 + "2" + 273);
+            Console.WriteLine("52" + 2 + 273);
+            Console.WriteLine(52 + 2 + "273");
+
+            int number = 52273;
+            string ouputA = number + "";
+
+            char character = 'a';
+            string ouputB = character + ""; //원래 character는 덧셈이 안됐었는데 업데이트 되면서 덧셈 가능
+            Console.WriteLine(ouputB);
+
+            Console.WriteLine(bool.Parse("True"));
+            Console.WriteLine(bool.Parse("true"));
+            Console.WriteLine(bool.Parse("TRUE"));
+            Console.WriteLine(bool.Parse("truE"));
+            Console.WriteLine(bool.Parse("TrUe"));
+
+            int outputInt = int.MinValue;
+            Console.WriteLine(outputInt);
+            Console.WriteLine(-(long)outputInt); //여기는 (long)으로 형변환해주고 해야지만 에러가 발생하지 않는다.
+            //Console.WriteLine(-(-2147483648));
         }
     }
 }
