939488ee6e29588bb4d60f7270d56a4df44954f7
수민 신 <ssoomin1@naver.com>
2021-05-04 11:59:56 +0900

    #14 switch -case문 실습 가급적으로는 상수가 돌아다니지 않게 할 것. 의미를 알 수 있도록 하는 것이 중요하다.

--- CSBasic2/Program.cs
+++ CSBasic2/Program.cs
@@ -10,6 +10,76 @@ namespace CSBasic2
     {
         static void Main(string[] args)
         {
+            if (DateTime.Now.Hour < 11)
+            {
+                Console.WriteLine("아침 먹을 시간입니다.");
+            }else if (DateTime.Now.Hour < 15)
+            {
+                Console.WriteLine("점심 먹을 시간입니다.");
+            }
+            else
+            {
+                Console.WriteLine("저녁 먹을 시간입니다.");
+            }
+
+            double score = 3.7;
+            if (score == 4.5)
+            {
+                Console.WriteLine("신");
+            }
+            else if (4.2 <= score)
+            {
+                Console.WriteLine("교수님의 사랑");
+            }
+            else if (3.5 <= score)
+            {
+                Console.WriteLine("협업");
+            }
+            else if (2.8 <= score)
+            {
+                Console.WriteLine("일반인");
+            }
+            else if (2.3 <= score)
+            {
+                Console.WriteLine("오락문화의 선구자");
+            }
+            else if (1.0 <= score)
+            {
+                Console.WriteLine("불가족천민");
+            }
+            else if (0.5 <= score)
+            {
+                Console.WriteLine("자벌레");
+            }
+            else if (0 <= score)
+                Console.WriteLine("플랑크톤");
+            else
+                Console.WriteLine("시대를 앞서가는 혁명의 씨앗");
+
+
+            Console.WriteLine("이번 달은 몇 월인가요: ");
+            int input = int.Parse(Console.ReadLine());
+            //맥락을 모르는 사람이 보면 의문을 가질수도 있음
+            //가급적으로 상수가 돌아다니지 않게 => 의미를 알 수 없는 Number
+            //의미를 알 수 있도록
+            switch (input)
+            {
+                case 12: case 1: case 2:
+                    Console.WriteLine("겨울");
+                    break;
+                case 3: case 4: case 5:
+                    Console.WriteLine("봄");
+                    break;
+                case 6: case 7: case 8:
+                    Console.WriteLine("여름");
+                    break;
+                case 9: case 10: case 11:
+                    Console.WriteLine("가을");
+                    break;
+                default:
+                    Console.WriteLine("지구에 있는거 맞나요?");
+                    break;
+            }
         }
     }
 }
