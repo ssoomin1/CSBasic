b7535977c4f20f22a6554ea25779021cdbede10c
수민 신 <ssoomin1@naver.com>
2021-05-06 10:08:22 +0900

    #16 while,do-while문 실습

--- CSBasic3/Program.cs
+++ CSBasic3/Program.cs
@@ -6,7 +6,29 @@ namespace CSBasic3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] intArray = new int[100];
+            Console.WriteLine(intArray.Length);
+            Console.WriteLine(intArray[0]);
+            Console.WriteLine(intArray[99]);
+            //Console.WriteLine(intArray[100]); //범위초과
+            //Console.WriteLine(intArray[-1]); //사용불가
+
+            int i = 0;
+            int[] intArray2 = { 52, 273, 32, 65, 103 };
+            while (i < intArray2.Length)
+            {
+                Console.WriteLine(i+"번째 출력: " + intArray2[i]);
+                i++;
+            }
+
+            string input;
+            do
+            {
+                Console.Write("입력(종료:eixt): ");
+                input = Console.ReadLine();
+            } while (input != "exit");
+        
         }
+        
     }
 }
