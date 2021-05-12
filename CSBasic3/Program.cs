de46092da108fa5f92f5e93d9f7c114187627277
수민 신 <ssoomin1@naver.com>
2021-05-13 08:49:07 +0900

    #18 for문과 역for문에 대한 실습

--- CSBasic3/Program.cs
+++ CSBasic3/Program.cs
@@ -27,7 +27,22 @@ namespace CSBasic3
                 Console.Write("입력(종료:eixt): ");
                 input = Console.ReadLine();
             } while (input != "exit");
-        
+            
+            for(int j = '가'; j <= '힣'; j++)
+            {
+                Console.Write((char)j);
+            }
+
+            int[] intArray3 = { 1, 2, 3, 4, 5, 6 };
+            for(int k = intArray3.Length - 1; k >= 0; i--)
+            {
+                Console.WriteLine(intArray3[k]);
+            }
+
+            for(int k = intArray3.Length; k > 0; k--)
+            {
+                Console.WriteLine(intArray3[k - 1]);
+            }
         }
         
     }
