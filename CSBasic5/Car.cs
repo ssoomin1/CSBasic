e8b08f5d53430268996568bf4ed9a4c2c1bedf06
수민 신 <ssoomin1@naver.com>
2021-06-22 12:21:33 +0900

    #25 메소드 기본형태에 대해서 학습

---
+++ CSBasic5/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSBasic5
+{
+    class Car
+    {
+        int carNumber;
+        DateTime inTime;
+        DateTime outTime;
+
+        public void SetIntime()
+        {
+            this.inTime = DateTime.Now;
+        }
+
+        public void SetoutTime()
+        {
+            this.outTime = DateTime.Now;
+        }
+    }
+}
--- CSBasic5/Program.cs
+++ CSBasic5/Program.cs
@@ -8,8 +8,60 @@ namespace CSBasic5
 {
     class Program
     {
+        class MethodExample
+        {
+            public int Power(int x)
+            {
+                return x + x;
+            }
+
+            public int Multi(int x,int y)
+            {
+                return x * y;
+            }
+
+            public void Print()
+            {
+                Console.WriteLine("Print() 메소드 호출됨");
+            }
+        }
+
         static void Main(string[] args)
         {
+
+            MethodExample me = new MethodExample();
+            Console.WriteLine(me.Power(10));
+            Console.WriteLine(me.Power(20));
+            Console.WriteLine(me.Multi(52,373));
+            Console.WriteLine(me.Multi(103,32));
+
+
+
+            Car car = new Car();
+            car.SetIntime();
+            car.SetoutTime();
+
+            Random random = new Random();
+            random.Next();
+            Console.WriteLine(random.Next(10, 100));
+            Console.WriteLine(random.Next(10, 100));
+            Console.WriteLine(random.Next(10, 100));
+            Console.WriteLine(random.Next(10, 100));
+            Console.WriteLine(random.Next(10, 100));
+            Console.WriteLine(random.Next(20));
+
+            Console.WriteLine(random.Next(20)+1);
+            Console.WriteLine(random.Next(1,21));
+
+            int[] intArray = new int[10];
+            long[] longArray = new long[10];
+            string[] stringArray = new string[10];
+
+            List<int> list = new List<int>();
+
+          
+
+
         }
     }
 }
